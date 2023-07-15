4dfc60ffb25e2b8566b7e74f08e75814bcaaf0ac
Destructor_Ben <ben.honeyfield@gmail.com>
2023-07-15 23:26:56 +1200

    Initial work on improved slot updating

---
+++ Content/AccessorySlots/CustomSlotPlayer.cs
@@ -0,0 +1,36 @@
+﻿namespace AccessoriesPlus.Content.AccessorySlots;
+internal class CustomSlotPlayer : ModPlayer
+{
+    public override void UpdateEquips()
+    {
+        InfoAccsUpdate();
+    }
+
+    public override void ResetInfoAccessories()
+    {
+        InfoAccsUpdate();
+    }
+
+    // Updating in inventory and vanity slots
+    public void InfoAccsUpdate()
+    {
+        // Vanity slots
+        for (int i = 13; i < 20; i++)
+        {
+            if (Player.IsItemSlotUnlockedAndUsable(i))
+                Player.RefreshInfoAccsFromItemType(Player.armor[i]);
+        }
+
+        // Inventory
+        foreach (var item in Player.inventory)
+        {
+            Player.RefreshInfoAccsFromItemType(item);
+        }
+    }
+
+    // TODO: Making other players share their mechanical accessories and building accessories effects
+    public override void RefreshInfoAccessoriesFromTeamPlayers(Player otherPlayer)
+    {
+        base.RefreshInfoAccessoriesFromTeamPlayers(otherPlayer);
+    }
+}
--- Content/AccessorySlots/CustomSlotSystem.cs
+++ Content/AccessorySlots/CustomSlotSystem.cs
@@ -3,6 +3,18 @@
 namespace AccessoriesPlus.Content.AccessorySlots;
 internal class CustomSlotSystem : GlobalItem
 {
+    // TODO: fill out
+    private static List<int> BuilderAccessories = new()
+    {
+        ItemID.HandOfCreation,
+    };
+
+    private static List<int> BuilderAccessoriesFromAccPlus = new()
+    {
+
+    };
+
+    // Forcing wings, etc. inmto modded slots
     public override bool CanEquipAccessory(Item item, Player player, int slot, bool modded)
     {
         var wingSlot = ModContent.GetInstance<WingSlot>();
@@ -11,13 +23,23 @@ internal class CustomSlotSystem : GlobalItem
 
         // Have to minus the number of accessory slots, since that is how many slots ahead the vanity one is, and subtracting it gets back to the original slot
         int vanitySlot = slot - player.GetModPlayer<ModAccessorySlotPlayer>().SlotCount;
-        if (Config.Instance.SlotForceWings && wingSlot.IsValidItem(item))
+        if (Config.Instance.SlotWings && Config.Instance.SlotForceWings && wingSlot.IsValidItem(item))
             return modded && (wingSlot.Type == slot || wingSlot.Type == vanitySlot);
-        else if (Config.Instance.SlotForceShields && shieldSlot.IsValidItem(item))
+        else if (Config.Instance.SlotShield && Config.Instance.SlotForceShields && shieldSlot.IsValidItem(item))
             return modded && (shieldSlot.Type == slot || shieldSlot.Type == vanitySlot);
-        else if (Config.Instance.SlotForceBoots && bootSlot.IsValidItem(item))
+        else if (Config.Instance.SlotBoots && Config.Instance.SlotForceBoots && bootSlot.IsValidItem(item))
             return modded && (bootSlot.Type == slot || bootSlot.Type == vanitySlot);
 
         return true;
+    }
+
+    // Making builder accessories work in the inventory
+    public override void UpdateInfoAccessory(Item item, Player player)
+    {
+        if (BuilderAccessories.Contains(item.type) || Config.Instance.ImprovedHandOfCreation && BuilderAccessoriesFromAccPlus.Contains(item.type))
+        {
+            Main.NewText(item.Name);
+            player.CopyVanillaEquipEffects(item.type, true);
+        }
     }
 }
