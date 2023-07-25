3699f41d5e6d7547bf0156eb6fc8412c1b2064ff
Destructor_Ben <ben.honeyfield@gmail.com>
2023-07-25 21:58:33 +1200

    Update AccessoriesPlus.cs

--- AccessoriesPlus.cs
+++ AccessoriesPlus.cs
@@ -4,5 +4,6 @@ internal class AccessoriesPlus : TerraUtilMod
     public static AccessoriesPlus Instance => ModContent.GetInstance<AccessoriesPlus>();
 
     // TODO: add workshop images
-    // TODO: make description more specific
+    // TODO: make description more specific in terms of what is actually changed because a lot of it is very generic
+    // TODO: make the description feature section look less bland
 }
